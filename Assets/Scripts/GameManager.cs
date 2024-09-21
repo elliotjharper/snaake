27ab8beb5464961907ab39eaafca734612974d95
Elliot Harper <elliotjharper@github>
2024-09-21 18:17:37 +0100

    Added tail segments and crashing into tails

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
     #region GameObjects
     public GameObject Square;
     public GameObject PlayerHead;
@@ -16,64 +17,70 @@ public class GameManager : MonoBehaviour {
     Vector3 foodPosition;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         // Calculate the cell size
         Config.CellSize = Config.GridSizeAbsolute / Config.GridColumns;
 
         // Create the grid
-        for (var xIndex = 0; xIndex < Config.GridColumns; xIndex++) {
-            for (var yIndex = 0; yIndex < Config.GridColumns; yIndex++) {
-                var instance = Instantiate(Square, position : GridToWorldPosition(xIndex, yIndex), Quaternion.identity, transform);
+        for (var xIndex = 0; xIndex < Config.GridColumns; xIndex++)
+        {
+            for (var yIndex = 0; yIndex < Config.GridColumns; yIndex++)
+            {
+                var instance = Instantiate(Square, position: GridToWorldPosition(xIndex, yIndex), Quaternion.identity, transform);
                 instance.transform.localScale = new Vector3(Config.CellSize, Config.CellSize);
             }
         }
 
         //create the player
-
-        var player = Instantiate(PlayerHead, position : GetRandomPosition(), Quaternion.identity, transform);
+        var player = Instantiate(PlayerHead, position: GetRandomPosition(), Quaternion.identity, transform);
         player.transform.localScale = new Vector3(Config.CellSize, Config.CellSize);
         player1 = player.GetComponent<PlayerHead>();
 
         // Create the food
-        foodInstance = Instantiate(Food, position : GetRandomPosition(), Quaternion.identity, transform);
+        foodInstance = Instantiate(Food, position: GetRandomPosition(), Quaternion.identity, transform);
         foodInstance.transform.localScale = new Vector3(Config.CellSize * 0.6f, Config.CellSize * 0.6f);
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         timeSinceLastTick += Time.deltaTime;
 
-        if (timeSinceLastTick >= TickInterval) {
+        if (timeSinceLastTick >= TickInterval)
+        {
             Tick();
             timeSinceLastTick -= TickInterval;
         }
     }
 
-    void Tick() {
-        player1.Tick();
-
-        if (player1.transform.position == foodInstance.transform.position) {
-            // Add tail to snake
-
+    void Tick()
+    {
+        if (player1.MoveOnTick(foodInstance.transform.position))
+        {
             RepositionFood();
         }
     }
 
-    void RepositionFood() {
+    void RepositionFood()
+    {
         foodInstance.transform.position = GetRandomPosition();
     }
 
-    Vector3 GetRandomPosition() {
+    Vector3 GetRandomPosition()
+    {
         var x = GetRandomGridCoordinate();
         var y = GetRandomGridCoordinate();
         return GridToWorldPosition(x, y);
     }
 
-    int GetRandomGridCoordinate() {
+    int GetRandomGridCoordinate()
+    {
         return Random.Range(0, Config.GridColumns - 1);
     }
 
-    Vector3 GridToWorldPosition(int x, int y) {
+    Vector3 GridToWorldPosition(int x, int y)
+    {
         return new Vector3((x + 0.5f) * Config.CellSize, (y + 0.5f) * Config.CellSize);
     }
 }
--- Assets/Scripts/PlayerHead.cs
+++ Assets/Scripts/PlayerHead.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerHead : MonoBehaviour {
+public class PlayerHead : MonoBehaviour
+{
+    public GameObject SnakeTail;
+
     #region Config
     public KeyCode UpKey;
     public KeyCode DownKey;
@@ -16,49 +20,88 @@ public class PlayerHead : MonoBehaviour {
 
     Vector3 bearing = Vector3.zero;
     Vector3 nextBearing = Vector3.zero;
+    SnakeTail child = null;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
 
     }
 
-    public void Tick() {
+    public bool MoveOnTick(Vector3 foodPosition)
+    {
         bearing = nextBearing;
 
-        if (bearing != Vector3.zero) {
+        if (bearing != Vector3.zero)
+        {
             var newPosition = transform.position + bearing * Config.CellSize;
 
-            if (isOutside(newPosition)) {
-                // Also check we haven't collided with ourself
+            if (isOutside(newPosition) || child?.NewPositionHitsTail(newPosition) == true)
+            {
                 SceneManager.LoadScene(GameOverSceneName);
-                return;
-            } else {
+                return false;
+            }
+            else
+            {
+                // store current position
+                var lastPosition = transform.position;
+
+                // move the head
                 transform.position += bearing * Config.CellSize;
+
+                if (!child)
+                {
+                    // create the first tail segment
+                    var snakeTailGameObject = Instantiate(SnakeTail, position: lastPosition, quaternion.identity);
+                    snakeTailGameObject.transform.localScale = new Vector3(Config.CellSize * 0.6f, Config.CellSize * 0.6f);
+                    child = snakeTailGameObject.GetComponent<SnakeTail>();
+                }
+
+                // figure out if we ate da food
+                var ateFood = transform.position == foodPosition;
+
+                // start the recursive tail movements
+                child.Move(lastPosition, ateFood);
+
+                return ateFood;
             }
         }
+
+        return false;
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKey(UpKey) && bearing != Vector3.down) {
+    void Update()
+    {
+        if (Input.GetKey(UpKey) && bearing != Vector3.down)
+        {
             nextBearing = Vector3.up;
-        } else if (Input.GetKey(DownKey) && bearing != Vector3.up) {
+        }
+        else if (Input.GetKey(DownKey) && bearing != Vector3.up)
+        {
             nextBearing = Vector3.down;
-        } else if (Input.GetKey(LeftKey) && bearing != Vector3.right) {
+        }
+        else if (Input.GetKey(LeftKey) && bearing != Vector3.right)
+        {
             nextBearing = Vector3.left;
-        } else if (Input.GetKey(RightKey) && bearing != Vector3.left) {
+        }
+        else if (Input.GetKey(RightKey) && bearing != Vector3.left)
+        {
             nextBearing = Vector3.right;
         }
 
     }
 
-    bool isOutside(Vector3 newPosition) {
-        if (newPosition.x < 0 || newPosition.y < 0) {
+    bool isOutside(Vector3 newPosition)
+    {
+        if (newPosition.x < 0 || newPosition.y < 0)
+        {
             return true;
         }
 
         var firstInvalidPosition = Config.GridSizeAbsolute;
-        if (newPosition.x >= firstInvalidPosition || newPosition.y >= firstInvalidPosition) {
+        if (newPosition.x >= firstInvalidPosition || newPosition.y >= firstInvalidPosition)
+        {
             return true;
         }
 

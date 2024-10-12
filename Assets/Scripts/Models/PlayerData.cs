564e5342309498328bec644ab925da6584712030
Elliot Harper <elliotjharper@github>
2024-10-12 13:59:22 +0100

    Initial DataStore

---
+++ Assets/Scripts/Models/PlayerData.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Numerics;
+
+public class PlayerData
+{
+    public int Id;
+    public Vector2 HeadPosition;
+    public List<Vector2> SegmentPositions;
+    public int Score;
+    public Vector2 NextBearing;
+    public int PlayerSpeed;
+}

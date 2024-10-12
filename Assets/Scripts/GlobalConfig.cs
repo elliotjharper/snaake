7eec9002f11ffb6c11d62265fd1da238daf7d5cf
Elliot Harper <elliotjharper@github>
2024-10-12 20:04:32 +0100

    Added the 'systems'

--- Assets/Scripts/GlobalConfig.cs
+++ Assets/Scripts/GlobalConfig.cs
@@ -2,8 +2,26 @@ using System.Reflection;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "GlobalConfig", menuName = "Scriptable Menu/Global Menu", order = 1)]
-public class GlobalConfig : ScriptableObject {
+public class GlobalConfig : ScriptableObject
+{
     public float GridSizeAbsolute;
     public int GridColumns;
     public float CellSize;
+
+    public Vector3 GetRandomPosition()
+    {
+        var x = GetRandomGridCoordinate();
+        var y = GetRandomGridCoordinate();
+        return GridToWorldPosition(x, y);
+    }
+
+    public int GetRandomGridCoordinate()
+    {
+        return Random.Range(0, GridColumns - 1);
+    }
+
+    public Vector3 GridToWorldPosition(int x, int y)
+    {
+        return new Vector3((x + 0.5f) * CellSize, (y + 0.5f) * CellSize);
+    }
 }
